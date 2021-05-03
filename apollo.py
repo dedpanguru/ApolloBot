f7ed4bbfa07fb0b870f10b99f3eb52d119c49f87
Gurv33r <70867602+Gurv33r@users.noreply.github.com>
2021-05-03 11:55:08 -0700

    Works for every course now!
    Using the http request pattern generated by search a course through SJSU's course descriptions search engine, this script can now search for any course, not just CS, SE, and CMPE courses anymore! You will get a couple runtime errors but the script will still work.

--- apollo.py
+++ apollo.py
@@ -1,7 +1,7 @@
 #import selenium webdriver
 from selenium import webdriver
-#import wait function
-from selenium.webdriver.support.ui import WebDriverWait 
+#import wait function from selenium webdriver support
+from selenium.webdriver.support.ui import WebDriverWait
 #import Opera Webdriver
 from webdriver_manager.opera import OperaDriverManager
 #import sys library for command-line processing
@@ -9,41 +9,45 @@ import sys
 
 #Extract specific course info after finding the course on the page
 def inner_table(driver):
-    """finds inner table element of the current element the global driver is on"""
     element = driver.find_elements_by_xpath(".//table[@class='td_dark']")
     if element:
         return element
     else:
         return False
 
-#Command-line Processing
-#Arguments must be in XX-XXX format, otherwise Timeout error ocurrs
-course = sys.argv[1]
-tokens = course.split('-')
-url = "https://catalog.sjsu.edu/preview_entity.php?catoid=2&ent_oid=130"
-if tokens[0] == "SE":
-    url = "https://catalog.sjsu.edu/preview_entity.php?catoid=1&ent_oid=18&p=2#courses"
-elif tokens[0] == "CMPE":
-    url = "https://catalog.sjsu.edu/preview_entity.php?catoid=1&ent_oid=18"
-course = course.replace("-", " ", 1)
+#Intake Command-line input 
+if len(sys.argv) > 3:
+    raise Exception("Invalid Argument!\n Format = <course department initials> <course number>")
+dept = sys.argv[1]
+number = sys.argv[2]
 
-#Open webdriver
+#Generate new url to webscrape from
+url = "https://catalog.sjsu.edu/content.php?filter%5B27%5D={dept}&filter%5B29%5D={number}&filter%5Bcourse_type%5D=-1&" \
+"filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=2&expand=&navoid=95&search_database=Filter&" \
+"filter%5Bexact_match%5D=1#acalog_template_course_filter".format(dept=dept, number=number)
+
+#Spin up new Opera window
 driver = webdriver.Opera(executable_path=OperaDriverManager().install())
-#Enter webpage
+
+#Go to the webpage
 driver.get(url)
-#Find course on the webpage
-elem = driver.find_elements_by_xpath("//*[contains(text(), '{}')]".format(course))
-#Course information is available only after clicking on the title, so click the title
-elem[0].click()
-#wait for the webpage to change, otherwise get a StaleElementException
-#Then, extract the inner information
+
+#Find course title
+elem = driver.find_elements_by_xpath("//*[contains(text(), '{}')]".format(tokens[0]+" "+tokens[1]))
+
+#Click on the course title element to access the course information
+if elem[0]:
+    elem[0].click()
+else:
+    try:
+        raise Exception()
+    except Exception:
+        print("Course not found!")
+        raise
+
 table = WebDriverWait(driver, 3).until(inner_table)
-#Format information
 lines = table[0].text.split('\n')
-#No need to keep driver open anymore, so close it
 driver.quit()
-#no need for last line of information
-lines.remove(lines[len(lines)-1])
-#print information
+lines.remove(lines[len(lines) - 1])
 for line in lines:
-    print(line)
+      print(line)
